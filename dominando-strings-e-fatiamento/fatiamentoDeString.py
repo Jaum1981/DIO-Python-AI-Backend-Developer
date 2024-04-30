e47ed9b2d4037146f22241ec1c0db85d184a74a1
João <juaun1981@alu.ufc.br>
2024-04-30 14:27:17 -0300

    add fatiamentoDeString.py

---
+++ dominando-strings-e-fatiamento/fatiamentoDeString.py
@@ -0,0 +1,16 @@
+#Fatiamento
+nome = "João Victor Amarante Diniz"
+
+nome[0] #>>> J
+
+nome[:9] #>>>"João Vict"
+
+nome[10:] #">>>r Amarante Diniz"
+
+nome[10:16] #>>>"r Amar"
+
+nome[10:16:2] #>>>"eAa"
+
+nome[:] #">>João Victor Amarante Diniz"
+
+nome[::-1] #>>>"ziniD etnaramA rotciV oãoJ"
