590fe31f35558db7d1ebabc44be08735b0003bb2
João <juaun1981@alu.ufc.br>
2024-04-30 09:03:06 -0300

    Adição de outros parâmetros

--- tipos-de-operadores/operadoresAritiméticos.py
+++ tipos-de-operadores/operadoresAritiméticos.py
@@ -4,8 +4,17 @@ print(1+1)
 #Subtração
 print(2-1)
 
+#Multiplicação
+print(2*2)
+
 #Divisão
 print(4/2)
 
-#Multiplicação
-print(2*2)
+#Divisão Inteira
+print(5//2)
+
+#Módulo
+print(10%3)
+
+#Exponenciação
+print(2**3)
