7bb45be54064f49eb9e67f9f55b62e4841c70411
João <juaun1981@alu.ufc.br>
2024-04-30 14:31:29 -0300

    add de stringsMultiplaLinhas.py

---
+++ dominando-strings-e-fatiamento/stringsMultiplaLinhas.py
@@ -0,0 +1,14 @@
+#Strings Triplas:
+
+nome = "Victor"
+
+mensagem = f"""
+    Ola, meu nome e {nome},
+estou aprendendo python
+ Essa mensagem tem diferentes recuos
+"""
+#Saida:
+#     Ola, meu nome e {nome},
+# estou aprendendo python
+#  Essa mensagem tem diferentes recuos
+
