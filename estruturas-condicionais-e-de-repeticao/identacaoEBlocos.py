98d536ec0053b4b3ad7def65ec65445e7cdfe804
João <juaun1981@alu.ufc.br>
2024-04-30 10:12:52 -0300

    criação da pasta de estruturas condicionais e de repetição, além da identaçãoe e blocos.py

---
+++ estruturas-condicionais-e-de-repeticao/identacaoEBlocos.py
@@ -0,0 +1,9 @@
+def sacar(self, valor: float) -> None: #inicio do bloco do metodo
+    
+    if self.saldo >- valor: #inicio do bloco If
+        
+        self.saldo -= valor
+        
+    #fim do bloco IF
+    
+#fim do bloco do metodo
