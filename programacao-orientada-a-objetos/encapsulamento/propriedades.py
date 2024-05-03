c14c51821185ff7981e0c669a1a3e34926f3bdb3
João <juaun1981@alu.ufc.br>
2024-05-03 15:12:45 -0300

    .

--- programacao-orientada-a-objetos/encapsulamento/propriedades.py
+++ programacao-orientada-a-objetos/encapsulamento/propriedades.py
@@ -4,11 +4,11 @@ class Foo:
     def __init__(self, x=None):
         self._x = x
         
-    @property
+    @property #transforma um metodo em propriedade
     def x(self):
         return self._x or 0
     
-    @x.setter
+    @x.setter #so assim para a linha23 funcionar daquele jeito
     def x(self, value):
         _x = self._x or 0
         _value = value or 0
@@ -19,8 +19,8 @@ class Foo:
         self._x = -1
         
 foo = Foo(10)
-print(foo.x)
-foo.x =10
+print(foo.x) #se eu nao tivesse colocado o @property retornaria o endereço
+foo.x = 10
 print(foo.x)
 del foo.x
 print(foo.x)
